c966a183261865a139f1db6309ddf80c5be868d8
Chaidirrahman <90771243+chaidir10@users.noreply.github.com>
2025-07-20 08:19:40 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
-
-self.addEventListener('install', function(e) {
+// service-worker.js
+self.addEventListener('install', function (e) {
+  console.log('Service Worker: Installed');
   e.waitUntil(
-    caches.open('absensi-cache').then(function(cache) {
+    caches.open('v1').then(function (cache) {
       return cache.addAll([
         '/',
-        '/aplikasi_absensi_pwa.html',
-        '/manifest.json'
+        '/absensi/index.html',
+        '/absensi/manifest.json',
+        '/absensi/icons/icon-192.png',
+        '/absensi/icons/icon-192-maskable.png',
+        '/absensi/icons/icon-512.png',
+        '/absensi/icons/icon-512-maskable.png',
+        // tambahkan semua file statis yang ingin di-cache
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
